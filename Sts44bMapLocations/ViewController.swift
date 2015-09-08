a9aaf9859acef871adc9f09462c24ce966b08150
Seanmichael Stanley <SeanmichaelStanley@mwc-016178.dhcp.missouri.edu>
2015-09-08 12:37:38 -0500

    Initial Commit

---
+++ Sts44bMapLocations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sts44bMapLocations
+//
+//  Created by Seanmichael Stanley on 9/8/15.
+//  Copyright (c) 2015 Seanmichael Stanley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
